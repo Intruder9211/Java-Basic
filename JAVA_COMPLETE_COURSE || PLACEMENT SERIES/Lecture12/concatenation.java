7f921ed0a0ca642771dc9e70b1fc9d56a2381a1f
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-06-28 14:17:41 +0000

    string

---
+++ JAVA_COMPLETE_COURSE || PLACEMENT SERIES/Lecture12/concatenation.java
@@ -0,0 +1,16 @@
+public class concatenation {
+    public static void main(String[] args) {
+        //concatenation add two or more strings
+        String first ="tony";
+        String last ="stark";
+        String full = first + " " +last;
+        System.out.println(full);
+         System.out.println(full.length());
+
+         //charAt
+         for (int i=0;i<full.length();i++){
+            System.out.println(full.charAt(i));
+         }
+    }
+    
+}
