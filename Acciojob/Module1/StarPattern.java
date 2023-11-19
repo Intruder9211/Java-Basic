2423c91405bbd9f43016563196d4768680998d33
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-11-19 12:59:27 +0530

    Create StarPattern.java

---
+++ Acciojob/Module1/StarPattern.java
@@ -0,0 +1,21 @@
+*
+***
+*****
+
+  import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) {
+        //your code here
+       int row =3;
+        
+for(int i=1; i<=row; i++)   
+{   
+for(int j=1; j<=i*2-1; j++)   
+{   
+System.out.print("*");   
+}   
+System.out.println();   
+    }
+}}
