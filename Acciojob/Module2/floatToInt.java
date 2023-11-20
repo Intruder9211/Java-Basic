b2b0ca53ea4671d274fe29718747fb4e8e33f1f1
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-11-20 20:23:42 +0530

    Create floatToInt.java

---
+++ Acciojob/Module2/floatToInt.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		//your code here
+        float v1 = 10.5f;
+        float v2 = 12.5f;
+        float v3 = 12.15f;
+         float average =(v1+v2+v3)/3;
+        int avg = (int)average;
+      
+        System.out.println(avg);
+	}
+}
