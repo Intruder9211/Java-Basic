350645c28c99c689ba72c86bcfed6163efb7dc56
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-11-20 20:27:56 +0530

    Create quoderentAndReminder.java

---
+++ Acciojob/Module2/quoderentAndReminder.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void divide(){
+        // Your code here. Declare the variables and print the output here.
+        int a = 1042;
+        int b = 7;
+        int quo = a/b;
+        int rem = a%b;
+        System.out.print(quo);
+        System.out.print(" "+rem);
+  }
+  
+    public static void main(String[] args) {
+        divide();      
+        System.out.println('\n');
+    }
+}
