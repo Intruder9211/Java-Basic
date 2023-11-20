aeb73cf2de1bae6d31ab20354c8ac4a203922b44
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-11-20 19:53:31 +0530

    Create Tables.java

---
+++ Acciojob/Module2/Tables.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+        int input = 8;
+        for(int i = 1;i<=10;i++){
+            System.out.println("8 x " +i+" = "+8*i);
+        }
+    }
+}
