22484533be4e785bbe57ffd90d0aa7ae1a9c2753
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-11-20 19:56:37 +0530

    Create VariablePractice.java

---
+++ Acciojob/Module2/VariablePractice.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // your code here
+        int var = 10;
+        System.out.println("My num is "+var);
+    }
+}
