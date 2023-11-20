9459b88f5adf5a1ff2da2f689a9c5520311d6f72
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-11-20 19:42:39 +0530

    Create sumOperator.java

---
+++ Acciojob/Module2/sumOperator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // write code here
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
