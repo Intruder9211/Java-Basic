36a1e8635420e70db1ee2daed92b836f484d690d
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-11-24 20:21:48 +0530

    Create NaturalNumbers.java

---
+++ Acciojob/Module4/NaturalNumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long N = scanner.nextLong();
+        scanner.close();
+        System.out.println((N * (N+1)) / 2);
+		
+    }
+}
